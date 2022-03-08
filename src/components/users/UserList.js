5aa9f624a92529a4408556b006ad4f1a6b70cb8e
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-07 17:50:55 -0800

    mapped over sampleUsers to render userListings

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,5 +1,18 @@
+import sampleUsers from "../../datasets/users";
+
+import UserListing from "./UserListing";
+
 const UserList = () => {
-  return <h1>This is a list of users</h1>;
+  return (
+    <div>
+      <h1>This is a list of users</h1>
+      <ul>
+        {sampleUsers.map((user) => {
+          return <UserListing key={user._id} user={user} />;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default UserList;
