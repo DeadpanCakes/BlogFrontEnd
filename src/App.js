e5328dfad054f0113ac3e423285fee07a317fe84
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-07 16:53:26 -0800

    imported placeholder elements and used those for routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+import Home from "./components/home/Home";
+import PostList from "./components/posts/PostList";
+import Post from "./components/posts/Post";
+import UserList from "./components/users/UserList";
+import User from "./components/users/User";
+
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="posts" element={<h1>This is a list of posts</h1>} />
-        <Route path="/posts/:postid" element={<h1>This is a post</h1>} />
-        <Route path="/users" element={<h1>This is a lit of users</h1>} />
-        <Route path="/users/:userid/" element={<h1>This is a user</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="posts" element={<PostList />} />
+        <Route path="/posts/:postid" element={<Post />} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/users/:userid/" element={<User />} />
       </Routes>
     </div>
   );
