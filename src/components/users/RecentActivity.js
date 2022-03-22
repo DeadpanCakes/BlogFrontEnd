136b6348dd274a711fcc8ee42e943aa707435ecd
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-22 15:08:51 -0700

    added style to timestamp

--- src/components/users/RecentActivity.js
+++ src/components/users/RecentActivity.js
@@ -4,11 +4,12 @@ import {format} from "date-fns";
 const RecentActivity = (props) => {
   const { activity } = props;
 
-  const styleClasses = "bg-pink-300 p-7 m-7 rounded-md"
+  const styleClasses = "bg-pink-300 p-7 m-7 rounded-md flex flex-1 flex-col"
+  const timeStyle = "self-end"
   return (
     <li className={styleClasses}>
       <ActivityPreview activity={activity} />
-      <p>on {format(activity.timestamp, "MM/dd/yyyy")} at {format(activity.timestamp, "hh:mm:ss a")}</p>
+      <p className={timeStyle}>on {format(activity.timestamp, "MM/dd/yyyy")} at {format(activity.timestamp, "hh:mm:ss a")}</p>
     </li>
   );
 };
