8b59e2987e5a96c1bbdb82453d00cc0bc4b20ca3
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-22 15:13:25 -0700

    added a link to redirect user to activity's url

--- src/components/users/RecentActivity.js
+++ src/components/users/RecentActivity.js
@@ -1,5 +1,6 @@
 import ActivityPreview from "./ActivityPreview";
 import {format} from "date-fns";
+import {Link} from "react-router-dom";
 
 const RecentActivity = (props) => {
   const { activity } = props;
@@ -7,9 +8,11 @@ const RecentActivity = (props) => {
   const styleClasses = "bg-pink-300 p-7 m-7 rounded-md flex flex-1 flex-col"
   const timeStyle = "self-end text-sm"
   return (
-    <li className={styleClasses}>
+    <li>
+      <Link to={activity.url} className={styleClasses}>
       <ActivityPreview activity={activity} />
       <p className={timeStyle}>on {format(activity.timestamp, "MM/dd/yyyy")} at {format(activity.timestamp, "hh:mm:ss a")}</p>
+      </Link>
     </li>
   );
 };
