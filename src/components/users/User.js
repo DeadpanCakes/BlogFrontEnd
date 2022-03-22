f5628802d2781b0b872728f87504ea59bedf0919
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-21 17:40:12 -0700

    rendered recent activity on user page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,28 @@
 import { useParams } from "react-router-dom";
 
 import sampleUsers from "../../datasets/users";
+import samplePosts from "../../datasets/posts";
+import sampleComments from "../../datasets/comments";
+import RecentActivity from "./RecentActivity";
 
 const User = () => {
   const { userid } = useParams();
   const userData = sampleUsers.find((user) => user._id.toString() === userid);
+  const publishedPosts = samplePosts.filter(post => post.isPublished)
+  const recentActivity = [...publishedPosts, ...sampleComments].filter(
+    (activity) => {
+      return activity.author._id === userData._id;
+    }
+  ).sort((a,b) => b.timestamp - a.timestamp);
   return (
     <div>
       <h2>{`${userData.username}#${userData._id}`}</h2>
       <p>{userData.fullName}</p>
+      <ul>
+        {recentActivity.map((activity) => {
+          return <RecentActivity key={activity._id} activity={activity} />;
+        })}
+      </ul>
     </div>
   );
 };
