5eb80318351d4744d087c6400a1bd5d3e5d84905
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-23 14:48:33 -0700

    added some styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,7 @@
 const Footer = () => {
-    return <footer>
+    const styleClasses = "bg-[#C2CAE8] min-h-[40vh] pt-10"
+
+    return <footer className={styleClasses}>
         <p>Copyright 2022</p>
     </footer>
 }
