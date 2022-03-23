3be6a30dcc67f9b431b418d231f763fa42610b51
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-23 13:41:21 -0700

    added comment list under post content

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,9 +1,19 @@
 import { useParams } from "react-router-dom";
 
+import CommentList from "../comment/CommentList";
+
 const Post = () => {
   const { postid } = useParams();
   console.log(postid);
-  return <h1>This is post {postid}</h1>;
+  return (
+    <div>
+      <h3>This is post {postid}</h3>
+      <div>
+        <h4>Comments</h4>
+        <CommentList />
+      </div>
+    </div>
+  );
 };
 
 export default Post;
