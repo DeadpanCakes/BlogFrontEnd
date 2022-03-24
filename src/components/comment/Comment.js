096776a9b08e5482cd4b48ec2587275fb84a6e23
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-24 15:13:17 -0700

    added link to user in each comment

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Comment = ({ comment }) => {
   const nestedComments = (comment.children || []).map((comment) => {
     return <Comment key={comment._id} comment={comment} />;
@@ -5,12 +7,12 @@ const Comment = ({ comment }) => {
   return (
     <li className="border-2 border-black">
       <div>
-        <h3>{comment.author.username}</h3>
+        <h3>
+          <Link to={comment.author.url}>{comment.author.username}</Link>
+        </h3>
         <p>{comment.content}</p>
       </div>
-      <ul>
-      {nestedComments}
-      </ul>
+      <ul>{nestedComments}</ul>
     </li>
   );
 };
