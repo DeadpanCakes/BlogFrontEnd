7c90d6340250943eed61be1492c7651f43729f9e
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-24 16:56:28 -0700

    copied the logic from sidebar into postList

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,5 +1,20 @@
-const PostList = () => {
-  return <h1>This is a list of posts</h1>;
+import { Link } from "react-router-dom";
+
+import samplePosts from "../../datasets/posts";
+
+const PostList = ({beginning, end}) => {
+  return samplePosts
+    .sort((prevPost, currPost) => {
+      return currPost.timestamp - prevPost.timestamp;
+    })
+    .slice(beginning, end)
+    .map((post) => {
+      return (
+        <li key={post._id}>
+          <Link to={post.url}>{post.title}</Link>
+        </li>
+      );
+    });
 };
 
 export default PostList;
