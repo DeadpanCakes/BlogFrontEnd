02162ee520c5017e7d7d92a32165b66321d03d1b
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-24 16:04:12 -0700

    made Sidebar component to render list act as a nav

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+
+import samplePosts from "../../datasets/posts";
+import sampleComments from "../../datasets/comments";
+
+const Sidebar = ({className}) => {
+  return (
+    <nav className={className}>
+      <Link to="/posts">All posts</Link>
+      <section>
+        <h4>Recent Posts</h4>
+        {samplePosts
+          .sort((prevPost, currPost) => {
+            return currPost.timestamp - prevPost.timestamp;
+          })
+          .slice(0, 2)
+          .map((post) => {
+            return (
+              <li key={post._id}>
+                <Link to={post.url}>{post.title}</Link>
+              </li>
+            );
+          })}
+      </section>
+      <section>
+        <h4>Recent Comments</h4>
+        {sampleComments
+          .sort((prevComment, currComment) => {
+            return currComment.timestamp - prevComment.timestamp;
+          })
+          .slice(0, 2)
+          .map((comment) => {
+            return (
+              <li key={comment._id}>
+                <Link to={comment.url}>{comment.content}</Link>
+              </li>
+            );
+          })}
+      </section>
+    </nav>
+  );
+};
+
+export default Sidebar;
