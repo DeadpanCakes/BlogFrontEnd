895cd75dfd92838c31942a14bef0839d55f7dba7
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-24 16:04:31 -0700

    added Sidebar and some structure to page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,18 @@
+import Sidebar from "../sidebar/Sidebar";
+
 const Home = () => {
-    return <h1>Home</h1>
+  const containerStyle = "flex p-20";
+    const mainStyle = "flex-1 p-10";
+    const sidebarStyle="flex-initial p-20";
+
+  return (
+    <div className={containerStyle}>
+      <main className={mainStyle}>
+        <h2>Home</h2>
+      </main>
+      <Sidebar className={sidebarStyle} />
+    </div>
+  );
 };
 
 export default Home;
