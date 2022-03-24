87dfd5f2eb86680c24cb07ec83df3c5feac37cfd
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-24 16:33:06 -0700

    added some styling

--- src/components/posts/Content.js
+++ src/components/posts/Content.js
@@ -1,7 +1,8 @@
 const Content = (props) => {
   const { content } = props;
+  const sectionStyle="min-h-[60vh]"
   return (
-    <section>
+    <section className={sectionStyle}>
       <p>{content}</p>
     </section>
   );
--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -11,30 +11,34 @@ const Post = () => {
     return post._id.toString() === postid;
   });
 
+  const containerStyle = "grid grid-cols-3 items-center";
+  const mainStyle = "col-span-2 p-10 pl-44 self-end"
   return (
-    <div>
-      <article>
-        <h2>{post.title}</h2>
+    <div className={containerStyle}>
+      <main className={mainStyle}>
+        <article>
+          <h2>{post.title}</h2>
+          <section>
+            <p>
+              By <Link to={post.author.url}>{post.author.fullName}</Link>
+            </p>
+            <span>
+              <p>In: </p>
+              <ul>
+                {post.tags.map((tag) => (
+                  <li>{tag}</li>
+                ))}
+              </ul>
+            </span>
+          </section>
+          <Content content={post.content} />
+        </article>
         <section>
-          <p>
-            By <Link to={post.author.url}>{post.author.fullName}</Link>
-          </p>
-          <span>
-            <p>In: </p>
-            <ul>
-              {post.tags.map((tag) => (
-                <li>{tag}</li>
-              ))}
-            </ul>
-          </span>
+          <h4>Comments</h4>
+          <CommentList />
         </section>
-        <Content content={post.content} />
-      </article>
+      </main>
       <Sidebar />
-      <section>
-        <h4>Comments</h4>
-        <CommentList />
-      </section>
     </div>
   );
 };
