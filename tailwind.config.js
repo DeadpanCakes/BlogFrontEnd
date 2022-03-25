d869b225a3591b98553c7d845ceda1dab8f295ea
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-24 17:46:58 -0700

    added some colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "darkBG": "#111D4A",
+      "highlight": "#45F0DF",
+      "darkFont" : "#C2CAE8"
+    },
     extend: {
       dropShadow: {
         "darkxl":  "0 0 10px rgba(194, 202, 232, 1)"
