674d57c636c4bd5e20e74a25d1afbe3088e480f2
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-28 14:54:23 -0700

    wrapped comments btwn ul

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = ({ sidebarStyle }) => {
   const containerStyle =
     "border-darkBG border-4 rounded-md flex flex-col min-h-[50vh]";
   const titleStyle = "text-3xl bg-highlight";
-  const sectionStyle = "flex-1 flex flex-col list-none text-left";
+  const sectionStyle = "flex-1 flex flex-col text-left";
   return (
     <nav className={sidebarStyle + " " + containerStyle}>
       <Link to="/posts" className={"text-4xl bg-darkBG text-darkFont"}>
@@ -19,18 +19,20 @@ const Sidebar = ({ sidebarStyle }) => {
       </section>
       <section className={sectionStyle}>
         <h4 className={titleStyle}>Recent Comments</h4>
-        {sampleComments
-          .sort((prevComment, currComment) => {
-            return currComment.timestamp - prevComment.timestamp;
-          })
-          .slice(0, 3)
-          .map((comment) => {
-            return (
-              <li key={comment._id}>
-                <Link to={comment.url}>{comment.content}</Link>
-              </li>
-            );
-          })}
+        <ul className="list-none">
+          {sampleComments
+            .sort((prevComment, currComment) => {
+              return currComment.timestamp - prevComment.timestamp;
+            })
+            .slice(0, 3)
+            .map((comment) => {
+              return (
+                <li key={comment._id}>
+                  <Link to={comment.url}>{comment.content}</Link>
+                </li>
+              );
+            })}
+        </ul>
       </section>
     </nav>
   );
