d9e31e878d12e0d0cb6b9c15dbcd5b8fe947e68b
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-28 10:39:35 -0700

    moved theme declarations into extend to preverve base theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      "darkBG": "#111D4A",
-      "highlight": "#45F0DF",
-      "darkFont" : "#C2CAE8"
-    },
     extend: {
       dropShadow: {
         "darkxl":  "0 0 10px rgba(194, 202, 232, 1)"
-      }
+      },
+      colors: {
+        "darkBG": "#111D4A",
+        "highlight": "#45F0DF",
+        "darkFont" : "#C2CAE8"
+      },
     },
   },
   plugins: [],
