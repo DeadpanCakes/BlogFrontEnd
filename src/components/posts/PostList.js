1e84f40909a27ab3282a829d2f7e68fec945b9c8
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-28 14:54:04 -0700

    wrapped mapped post components between ul

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -2,19 +2,24 @@ import { Link } from "react-router-dom";
 
 import samplePosts from "../../datasets/posts";
 
-const PostList = ({beginning, end}) => {
-  return samplePosts
-    .sort((prevPost, currPost) => {
-      return currPost.timestamp - prevPost.timestamp;
-    })
-    .slice(beginning, end)
-    .map((post) => {
-      return (
-        <li key={post._id}>
-          <Link to={post.url}>{post.title}</Link>
-        </li>
-      );
-    });
+const PostList = ({ beginning, end }) => {
+  const listStyle = "list-none";
+  return (
+    <ul className={listStyle}>
+      {samplePosts
+        .sort((prevPost, currPost) => {
+          return currPost.timestamp - prevPost.timestamp;
+        })
+        .slice(beginning, end)
+        .map((post) => {
+          return (
+            <li key={post._id}>
+              <Link to={post.url}>{post.title}</Link>
+            </li>
+          );
+        })}
+    </ul>
+  );
 };
 
 export default PostList;
