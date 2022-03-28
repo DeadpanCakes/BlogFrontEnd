540edab440a8f8822ec5cd9b2c92ecb2fe7473db
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-28 16:35:03 -0700

    made basic layout of AllPosts

--- src/components/posts/AllPosts.js
+++ src/components/posts/AllPosts.js
@@ -1,8 +1,26 @@
 import PostList from "./PostList";
 
 const AllPosts = () => {
-    return <section>
-        <PostList />
-    </section>
-}
-export default AllPosts
+  const containerStyle = "flex px-40 py-10";
+  const titleStyle = "py-10"
+  const controlStyle = "max-w-[20vw] min-w-[10vw]";
+  const postsStyle = "flex-1";
+  return (
+    <div>
+      <h2 className={titleStyle}>All Posts</h2>
+      <section className={containerStyle}>
+        <div className={controlStyle}>
+          <ul>
+            <li>Filter</li>
+            <li>Sort</li>
+            <li>Search</li>
+          </ul>
+        </div>
+        <div className={postsStyle}>
+          <PostList />
+        </div>
+      </section>
+    </div>
+  );
+};
+export default AllPosts;
