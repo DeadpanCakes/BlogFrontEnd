fc51b94e7f0ddf9d785fe31987074274e2829e0f
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-28 15:20:50 -0700

    added a dropshadow for the light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      dropShadow: {
+      dropShadow: { 
+        "light": "0 0 5px #8789C0",
         "darkxl":  "0 0 10px rgba(194, 202, 232, 1)"
       },
       colors: {
