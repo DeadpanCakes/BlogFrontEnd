a76bc76c3b3a1930c69880460686d701ceda76ad
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-28 15:20:31 -0700

    added styles to Link and text in Link

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -4,6 +4,9 @@ import samplePosts from "../../datasets/posts";
 
 const PostList = ({ beginning, end }) => {
   const listStyle = "list-none";
+  const linkStyle = "flex flex-col px-5"
+  const authorCredit = "self-end"
+  const postTitleStyle = "hover:underline"
   return (
     <ul className={listStyle}>
       {samplePosts
@@ -14,7 +17,10 @@ const PostList = ({ beginning, end }) => {
         .map((post) => {
           return (
             <li key={post._id}>
-              <Link to={post.url}>{post.title}</Link>
+              <Link to={post.url} className={linkStyle}>
+                <p className={postTitleStyle}>{post.title}</p>
+                <p className={authorCredit}>by {post.author.username}</p>
+              </Link>
             </li>
           );
         })}
