1e706e4863071f2ae0257941912a9c9f1c30d872
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-28 16:44:33 -0700

    resized title

--- src/components/posts/AllPosts.js
+++ src/components/posts/AllPosts.js
@@ -2,8 +2,8 @@ import PostList from "./PostList";
 
 const AllPosts = () => {
   const containerStyle = "flex pl-60 pr-40 py-10 min-h-[100vh]";
-  const titleStyle = "py-10"
-  const controlStyle = "flex-1 border-2 border-darkBG max-w-[25vw]";
+  const titleStyle = "py-10 text-4xl"
+  const controlStyle = "flex-1 border-2 border-darkBG max-w-[20vw]";
   const postsStyle = "flex-1";
   return (
     <div>
