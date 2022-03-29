983fc99a7545092099cf9cea7400cf16f4b4c874
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-28 17:55:06 -0700

    imported HasiLink to jump to a comment when linked to one

--- src/components/users/RecentActivity.js
+++ src/components/users/RecentActivity.js
@@ -1,18 +1,32 @@
 import ActivityPreview from "./ActivityPreview";
-import {format} from "date-fns";
-import {Link} from "react-router-dom";
+import { format } from "date-fns";
+import { HashLink } from "react-router-hash-link";
+
+import identifyDoc from "../../utils/identifyDoc";
 
 const RecentActivity = (props) => {
   const { activity } = props;
 
-  const styleClasses = "bg-pink-300 p-7 m-7 rounded-md flex flex-1 flex-col"
-  const timeStyle = "self-end text-sm"
+  const docType = identifyDoc(activity);
+
+  const styleClasses = "bg-pink-300 p-7 m-7 rounded-md flex flex-1 flex-col";
+  const timeStyle = "self-end text-sm";
   return (
     <li>
-      <Link to={activity.url} className={styleClasses}>
-      <ActivityPreview activity={activity} />
-      <p className={timeStyle}>on {format(activity.timestamp, "MM/dd/yyyy")} at {format(activity.timestamp, "hh:mm:ss a")}</p>
-      </Link>
+      <HashLink
+        to={
+          docType === "comment"
+            ? `${activity.url}#${activity._id}`
+            : activity.url
+        }
+        className={styleClasses}
+      >
+        <ActivityPreview activity={activity} />
+        <p className={timeStyle}>
+          on {format(activity.timestamp, "MM/dd/yyyy")} at{" "}
+          {format(activity.timestamp, "hh:mm:ss a")}
+        </p>
+      </HashLink>
     </li>
   );
 };
