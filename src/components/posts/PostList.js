cb6bc2ec09ad1f865bf05ded63d4e858e36b37b9
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-03-28 17:33:12 -0700

    restructured tho links so clicking on username links to profile

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -6,7 +6,7 @@ import samplePosts from "../../datasets/posts";
 const PostList = ({ beginning, end }) => {
   const listStyle = "list-none";
   const linkStyle = "flex flex-col px-5";
-  const authorCredit = "self-end";
+  const authorCredit = "self-end hover:underline";
   const postTitleStyle = "text-2xl hover:underline";
   return (
     <ul className={listStyle}>
@@ -17,10 +17,12 @@ const PostList = ({ beginning, end }) => {
         .slice(beginning, end)
         .map((post) => {
           return (
-            <li key={post._id}>
-              <Link to={post.url} className={linkStyle}>
+            <li key={post._id} className={linkStyle}>
+              <Link to={post.url}>
                 <p className={postTitleStyle}>{post.title}</p>
-                <p className={authorCredit}>
+              </Link>
+              <Link to={post.author.url} className={authorCredit}>
+                <p>
                   by {post.author.username} on{" "}
                   {format(post.timestamp, "MM/dd/yyyy")} at
                   {format(post.timestamp, "hh:mm:ss a")}
