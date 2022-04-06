850b27719eea545d2affef1f538714eda77998cb
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-05 17:56:18 -0700

    passed down login method and redirect from page when logged in

--- src/components/authentication/LoginPage.js
+++ src/components/authentication/LoginPage.js
@@ -1,18 +1,34 @@
-import LoginForm from "./LoginForm";
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const LoginPage = () => {
+import AuthContext from "../context/AuthContext";
+import LoginForm from "./LoginForm";
+
+const LoginPage = ({ isLogged }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isLogged) {
+      navigate("/");
+    }
+  }, [isLogged, navigate]);
+
   return (
-    <div>
-      <h2>Log In</h2>
-      <LoginForm />
-      <div>
-        <h3>New User?</h3>
-        <Link to="/sign-up">
-          <button>Sign Up</button>
-        </Link>
-      </div>
-    </div>
+    <AuthContext.Consumer>
+      {(auth) => {
+        return (
+          <div>
+            <h2>Log In</h2>
+            <LoginForm login={auth.loginUser} />
+            <div>
+              <h3>New User?</h3>
+              <Link to="/sign-up">
+                <button>Sign Up</button>
+              </Link>
+            </div>
+          </div>
+        );
+      }}
+    </AuthContext.Consumer>
   );
 };
 
