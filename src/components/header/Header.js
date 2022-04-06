7b9c0b71efda43752d737394b9718293675c301d
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-06 16:45:56 -0700

    imported profile widget and conditionally render when logged in

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 
-const Header = () => {
+import Profile from "./Profile";
+
+const Header = ({loggedUser}) => {
   const styleClasses = "flex p-5 bg-[#111D4A] text-darkFont text-2xl";
-  const logoStyle = "hover:drop-shadow-darkxl"
+  const logoStyle = "hover:drop-shadow-darkxl";
 
   return (
     <header className={styleClasses}>
       <Link className={logoStyle} to="/">
         <h1>Blog</h1>
       </Link>
+      {loggedUser ? <Profile /> : <button>Log In</button>}
     </header>
   );
 };
