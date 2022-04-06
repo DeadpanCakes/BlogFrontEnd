d0ddd6297661db6bec6bf37fccf6d8b339924dc9
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-05 17:55:26 -0700

    wrapped authcontext around app and stored auth state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
+import AuthContext from "./components/context/AuthContext";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
@@ -13,24 +16,63 @@ import LoginPage from "./components/authentication/LoginPage";
 import SignUpPage from "./components/authentication/SignUpPage";
 
 function App() {
+  const [loggedUser, setLoggedUser] = useState(localStorage.getItem("jwt"));
+  const loginUser = (jwt) => {
+    const user = jwtDecode(jwt);
+    console.log(user);
+    setLoggedUser({
+      _id: user._id,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    });
+  };
+  const logoutUser = () => {
+    setLoggedUser(null);
+    localStorage.removeItem("jwt");
+  };
+  const authData = { loggedUser, loginUser, logoutUser };
+  useEffect(() => {
+    const jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      const exp = new Date(jwtDecode(jwt).exp * 1000);
+      const currTime = new Date();
+      if (currTime > exp) {
+        logoutUser();
+      }
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <Header />
-      <div className="min-h-[85vh]">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<AllPosts />} />
-          <Route path="/posts/:postid" element={<Post />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/users/:userid/" element={<User />} />
-          <Route path="/posts/:postid/comments" element={<CommentList />} />
-          <Route path="/posts/:postid/comments/:commentid" element={<Post />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-        </Routes>
+    <AuthContext.Provider value={authData}>
+      <div className="App">
+        <Header />
+        <div className="min-h-[85vh]">
+          <button
+            onClick={() => {
+              logoutUser();
+            }}
+          >
+            Log Out
+          </button>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<AllPosts />} />
+            <Route path="/posts/:postid" element={<Post />} />
+            <Route path="/users" element={<UserList />} />
+            <Route path="/users/:userid/" element={<User />} />
+            <Route path="/posts/:postid/comments" element={<CommentList />} />
+            <Route
+              path="/posts/:postid/comments/:commentid"
+              element={<Post />}
+            />
+            <Route path="/login" element={<LoginPage isLogged={authData.loggedUser} />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </AuthContext.Provider>
   );
 }
 
