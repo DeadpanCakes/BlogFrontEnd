ad245939ddcd3b7e73d0ab82e9a69f94eb9896de
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-06 17:05:53 -0700

    made useUser to fetch users and return as array

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { User } from "../interfaces/User";
+
+const useUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    //Fetch users, store in state
+    const fetchUsers = async () => {
+      const response = await fetch(
+        "https://still-depths-86703.herokuapp.com/api/users"
+      );
+      setUsers(await response.json());
+    };
+    fetchUsers();
+  }, []);
+
+  const findUser = (targetID: String) => {
+    return users.find((user) => user._id === targetID);
+  };
+
+  return { users, findUser };
+};
+
+export default useUsers;
