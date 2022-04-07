aa48411016444e123fee9974991f7d0e12c029e5
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-07 15:34:53 -0700

    imported dropdown and conditionally render

--- src/components/header/Profile.js
+++ src/components/header/Profile.js
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
 
 import useUsers from "../../hooks/useUsers";
+import ProfileDropDown from "./ProfileDropDown";
 
 const Profile = ({ loggedUser }) => {
   const { users, findUser } = useUsers();
   const [user, setUser] = useState(null);
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prevState) => !prevState);
+
   useEffect(() => {
     setUser(findUser(loggedUser._id));
   }, [users, findUser, loggedUser]);
-  return <button>{user ? `Welcome, ${user.firstName}` : "Loading"}</button>;
+  return (
+    <div className="relative">
+      <button onClick={toggleClicked}>
+        {user ? `Welcome, ${user.firstName}` : "Loading"}
+      </button>
+      <ProfileDropDown isDisplayed={clicked} user={{_id: 1}}/>
+    </div>
+  );
 };
 
 export default Profile;
