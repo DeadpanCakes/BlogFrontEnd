eeeebeb94a2c827d08d76ccf450fefc3a663b40c
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-11 17:17:58 -0700

    added AuthContext to give dropdown user info and logout fn

--- src/components/header/ProfileDropDown.js
+++ src/components/header/ProfileDropDown.js
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
 
-const ProfileDropDown = ({ user, isDisplayed }) => {
+import AuthContext from "../context/AuthContext";
+
+const ProfileDropDown = ({ isDisplayed }) => {
   const containerStyle = "absolute right-0.5";
   const display = isDisplayed ? "flex flex-col" : "hidden";
   const navStyle = "bg-darkBG border-2 px-10 rounded-sm " + display;
 
   return (
-    <div className={containerStyle}>
-      <nav className={navStyle}>
-        <Link to={`/users/${user._id}`}>Profile</Link>
-        <Link to="/">Posts</Link>
-        <button>Logout</button>
-      </nav>
-    </div>
+    <AuthContext.Consumer>
+      {(authData) => {
+        const { loggedUser, logoutUser } = authData;
+        return (
+          <div className={containerStyle}>
+            <nav className={navStyle}>
+              <Link to={`/users/${loggedUser._id}`}>Profile</Link>
+              <Link to="/">Posts</Link>
+              <button onClick={logoutUser}>Logout</button>
+            </nav>
+          </div>
+        );
+      }}
+    </AuthContext.Consumer>
   );
 };
 
