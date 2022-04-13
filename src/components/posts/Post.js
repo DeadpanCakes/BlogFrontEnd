c1deeff2e17fef26479f6d718ffad91f73f3429c
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-12 19:43:52 -0700

    replaced samplePosts w/ usePosts

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,48 +1,62 @@
+import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 
 import CommentList from "../comment/CommentList";
 import Content from "./Content";
 import Sidebar from "../sidebar/Sidebar";
-import samplePosts from "../../datasets/posts";
+import usePosts from "../../hooks/usePosts";
 
 const Post = () => {
   const { postid } = useParams();
-  const post = samplePosts.find((post) => {
-    return post._id.toString() === postid;
-  });
+  const posts = usePosts();
+  const [post, setPost] = useState(null);
+  useEffect(() => {
+    setPost(
+      posts.find((post) => {
+        return post._id === postid;
+      })
+    );
+  }, [postid, posts]);
 
   const containerStyle = "grid grid-cols-3 items-center p-10 pr-44 gap-10";
-  const mainStyle = "col-span-2 p-10 pl-44 self-end"
-  const sidebarStyle = "self-start"
-  const commentsStyle = "text-left"
-  const commentTitleStyle = "border-darkBG border-y-2 text-2xl"
+  const mainStyle = "col-span-2 p-10 pl-44 self-end";
+  const sidebarStyle = "self-start";
+  const commentsStyle = "text-left";
+  const commentTitleStyle = "border-darkBG border-y-2 text-2xl";
 
   return (
     <div className={containerStyle}>
-      <main className={mainStyle}>
-        <article>
-          <h2>{post.title}</h2>
-          <section>
-            <p>
-              By <Link to={post.author.url}>{post.author.fullName}</Link>
-            </p>
-            <span>
-              <p>In: </p>
-              <ul>
-                {post.tags.map((tag) => (
-                  <li key={"tag"+tag}>{tag}</li>
-                ))}
-              </ul>
-            </span>
+      {post ? (
+        <main className={mainStyle}>
+          <article>
+            <h2>{post.title}</h2>
+            <section>
+              <p>
+                By{" "}
+                <Link to={`/users/${post.author._id}`}>
+                  {`${post.author.firstName} ${post.author.lastName}`}
+                </Link>
+              </p>
+              <span>
+                <p>In: </p>
+                <ul>
+                  {post.tags.map((tag) => (
+                    <li key={"tag" + tag}>{tag}</li>
+                  ))}
+                </ul>
+              </span>
+            </section>
+            <Content content={post.content} sidebarStyle={sidebarStyle} />
+          </article>
+          <section className={commentsStyle}>
+            <h4 className={commentTitleStyle}>Comments</h4>
+            <CommentList />
           </section>
-          <Content content={post.content} sidebarStyle={sidebarStyle} />
-        </article>
-        <section className={commentsStyle}>
-          <h4 className={commentTitleStyle}>Comments</h4>
-          <CommentList />
-        </section>
-      </main>
-      <Sidebar sidebarStyle={sidebarStyle}/>
+        </main>
+      ) : (
+        <div>Loading...</div>
+      )}
+      <Sidebar sidebarStyle={sidebarStyle} />
     </div>
   );
 };
