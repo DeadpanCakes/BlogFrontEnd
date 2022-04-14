ce839c246080c3b2a2eda2e80b410b94a058793b
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-14 16:32:11 -0700

    reworked HashLink.to to utilize activity.url

--- src/components/users/RecentActivity.js
+++ src/components/users/RecentActivity.js
@@ -2,26 +2,15 @@ import ActivityPreview from "./ActivityPreview";
 import { format } from "date-fns";
 import { HashLink } from "react-router-hash-link";
 
-import identifyDoc from "../../utils/identifyDoc";
-
 const RecentActivity = (props) => {
   const { activity } = props;
   const timestamp = new Date(activity.timestamp);
-
-  const docType = identifyDoc(activity);
 
   const styleClasses = "bg-pink-300 p-7 m-7 rounded-md flex flex-1 flex-col";
   const timeStyle = "self-end text-sm";
   return (
     <li>
-      <HashLink
-        to={
-          docType === "comment"
-            ? `/posts/${activity.commentOf._id}/comments${activity._id}#${activity._id}`
-            : `/posts/${activity._id}`
-        }
-        className={styleClasses}
-      >
+      <HashLink to={activity.fetchUrl} className={styleClasses}>
         <ActivityPreview activity={activity} />
         <p className={timeStyle}>
           on {format(timestamp, "MM/dd/yyyy")} at{" "}
