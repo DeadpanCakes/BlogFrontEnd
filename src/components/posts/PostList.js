ecfa666576cf54cc0696b7003050f6333c47ad64
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-14 17:06:54 -0700

    replaced samplePosts with usePosts

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
 
-import samplePosts from "../../datasets/posts";
+import usePosts from "../../hooks/usePosts";
 
 const PostList = ({ beginning, end }) => {
+  const posts = usePosts();
+
   const listStyle = "list-none";
   const linkStyle = "flex flex-col px-5";
   const authorCredit = "self-end hover:underline";
   const postTitleStyle = "text-2xl hover:underline";
   return (
     <ul className={listStyle}>
-      {samplePosts
+      {posts
         .sort((prevPost, currPost) => {
           return currPost.timestamp - prevPost.timestamp;
         })
@@ -18,14 +20,14 @@ const PostList = ({ beginning, end }) => {
         .map((post) => {
           return (
             <li key={post._id} className={linkStyle}>
-              <Link to={post.url}>
+              <Link to={post.fetchUrl}>
                 <p className={postTitleStyle}>{post.title}</p>
               </Link>
               <Link to={post.author.url} className={authorCredit}>
                 <p>
                   by {post.author.username} on{" "}
-                  {format(post.timestamp, "MM/dd/yyyy")} at
-                  {format(post.timestamp, "hh:mm:ss a")}
+                  {format(new Date(post.timestamp), "MM/dd/yyyy")} at{" "}
+                  {format(new Date(post.timestamp), "hh:mm:ss a")}
                 </p>
               </Link>
             </li>
