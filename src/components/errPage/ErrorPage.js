fb6ab031d5a748caf939c16baf2867f580ff86c1
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-20 11:11:48 -0700

    Added a btn to go back to page before bug occurred

--- src/components/errPage/ErrorPage.js
+++ src/components/errPage/ErrorPage.js
@@ -1,14 +1,19 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const buttonStyle = "rounded-md min-h-[50px] min-w-[100px] bg-darkBG m-5 text-darkFont"
   return (
     <>
       <h1>Whoops</h1>
       {location.state ? <p>{location.state.msg}</p> : <p>Something happened</p>}
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+      <div>
+        <button className={buttonStyle} onClick={() => navigate(-2)}>Go Back</button>
+        <Link to="/">
+          <button className={buttonStyle}>Home</button>
+        </Link>
+      </div>
     </>
   );
 };
