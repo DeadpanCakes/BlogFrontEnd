0e670b5a9a1da212fb399e94a0e70d5ad309baba
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-25 16:39:09 -0700

    pulled in state to pass down to multiple children

--- src/components/comment/CommentSection.js
+++ src/components/comment/CommentSection.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
 
+import useComments from "../../hooks/useComments";
 import CommentList from "./CommentList";
 import CommentForm from "./CommentForm";
 
 const CommentSection = ({ postid }) => {
+  //Style
   const containerStyle = "text-left";
   const commentTitleStyle = "border-darkBG border-y-2 text-2xl";
+
+  //Init State
+  const commentData = useComments(postid);
   const [replyingTo, setReply] = useState(null);
+  const [formContent, setContent] = useState("");
+  const [editTarget, setEditing] = useState(null);
 
   return (
     <section className={containerStyle}>
@@ -15,9 +22,16 @@ const CommentSection = ({ postid }) => {
         postid={postid}
         replyingTo={replyingTo}
         setReply={setReply}
+        formContent={formContent}
+        setContent={setContent}
+        editTarget={editTarget}
       />
-      <CommentList setReply={setReply} />
-      <button onClick={() => console.log(replyingTo)}>test</button>
+      <CommentList
+        commentData={commentData}
+        setReply={setReply}
+        setContent={setContent}
+        setEditing={setEditing}
+      />
     </section>
   );
 };
