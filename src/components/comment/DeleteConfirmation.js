8e1d763905f613e7652fa8f7efcecb2b7b3af651
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-26 10:06:58 -0700

    added functionality to deleteConfirmation to actually delete comment

--- src/components/comment/DeleteConfirmation.js
+++ src/components/comment/DeleteConfirmation.js
@@ -1,12 +1,41 @@
-import Comment from "./Comment";
+import { useNavigate, useLocation } from "react-router-dom";
 
-const DeleteConfirmation = ({ comment }) => {
+import Comment from "./Comment";
+import deleteData from "../../utils/deleteData";
+import getBearerToken from "../../utils/getBearerToken";
+
+const DeleteConfirmation = () => {
+  const commentStyle = "max-w-[50%] ";
+  const location = useLocation();
+  const navigate = useNavigate();
+  const comment = location.state;
+
   return (
     <div>
       <h2>Delete Confirmation</h2>
       <div>
         <p>Are you sure you want to delete {comment._id}</p>
-        <Comment comment={comment} />
+        <div className={commentStyle}>
+          <Comment comment={comment} isPreview={true} />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              const postid = comment.commentOf._id;
+              deleteData(
+                `https://still-depths-86703.herokuapp.com/api/posts/${postid}/comments/${comment._id}`,
+                getBearerToken()
+              ).then((res) => {
+                res.json().then((data) => {
+                  navigate(data.post.fetchUrl);
+                });
+              });
+            }}
+          >
+            Yes
+          </button>
+          <button>No</button>
+        </div>
       </div>
     </div>
   );
