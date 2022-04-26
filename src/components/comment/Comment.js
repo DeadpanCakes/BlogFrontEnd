911bffdc8cd48732bdc8888eb1fc201da46872c0
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-25 17:10:37 -0700

    extracted CommentCtrls into own component

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -1,66 +1,33 @@
 import { Link } from "react-router-dom";
 import parseHtml from "html-react-parser";
 
-import AuthContext from "../context/AuthContext";
+import CommentCtrls from "./CommentCtrls";
 
 const Comment = ({ comment, setReply, setContent, setEditing }) => {
+  const nestedComments = (comment.children || []).map((comment) => {
+    return <Comment key={comment._id} comment={comment} setReply={setReply} />;
+  });
   return (
-    <AuthContext.Consumer>
-      {(auth) => {
-        const handleEdit = () => {
-          setContent(parseHtml(comment.content));
-          setEditing(comment._id);
-          document.getElementById("commentForm").scrollIntoView();
-        };
-        const handleReply = () => {
-          setReply({
-            id: comment._id,
-            author: comment.author,
-            content: comment.content,
-          });
-          document.getElementById("commentForm").scrollIntoView();
-        };
-
-        const nestedComments = (comment.children || []).map((comment) => {
-          return (
-            <Comment key={comment._id} comment={comment} setReply={setReply} />
-          );
-        });
-        return (
-          <li
-            id={comment._id}
-            className="pt-5 pl-5 border-l border-darkBG bg-[#C2CAE8]"
-          >
-            <div>
-              <h3>
-                <Link to={comment.author.fetchUrl}>
-                  {comment.author.username}
-                </Link>
-              </h3>
-              <div
-                dangerouslySetInnerHTML={{ __html: parseHtml(comment.content) }}
-              ></div>
-            </div>
-            <div>
-              {auth.loggedUser ? (
-                auth.loggedUser._id === comment.author._id ? (
-                  <>
-                    <button onClick={handleEdit}>Edit</button>
-                    <Link to={`/comments/${comment._id}/delete`}>
-                      <button>Delete</button>
-                    </Link>
-                    <button onClick={handleReply}>Reply</button>
-                  </>
-                ) : (
-                  <button onClick={handleReply}>Reply</button>
-                )
-              ) : null}
-            </div>
-            <ul>{nestedComments}</ul>
-          </li>
-        );
-      }}
-    </AuthContext.Consumer>
+    <li
+      id={comment._id}
+      className="pt-5 pl-5 border-l border-darkBG bg-[#C2CAE8]"
+    >
+      <div>
+        <h3>
+          <Link to={comment.author.fetchUrl}>{comment.author.username}</Link>
+        </h3>
+        <div
+          dangerouslySetInnerHTML={{ __html: parseHtml(comment.content) }}
+        ></div>
+      </div>
+      <CommentCtrls
+        comment={comment}
+        setReply={setReply}
+        setContent={setContent}
+        setEditing={setEditing}
+      />
+      <ul>{nestedComments}</ul>
+    </li>
   );
 };
 
---
+++ src/components/comment/CommentCtrls.js
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import parseHtml from "html-react-parser";
+
+import AuthContext from "../context/AuthContext";
+
+const CommentCtrls = ({ comment, setContent, setReply, setEditing }) => {
+  const handleEdit = () => {
+    setContent(parseHtml(comment.content));
+    setEditing(comment._id);
+    document.getElementById("commentForm").scrollIntoView();
+  };
+  const handleReply = () => {
+    setReply({
+      id: comment._id,
+      author: comment.author,
+      content: comment.content,
+    });
+    document.getElementById("commentForm").scrollIntoView();
+  };
+  return (
+    <AuthContext.Consumer>
+      {(auth) => {
+        return (
+          <div>
+            {auth.loggedUser ? (
+              auth.loggedUser._id === comment.author._id ? (
+                <>
+                  <button onClick={handleEdit}>Edit</button>
+                  <Link to={`/comments/${comment._id}/delete`}>
+                    <button>Delete</button>
+                  </Link>
+                  <button onClick={handleReply}>Reply</button>
+                </>
+              ) : (
+                <button onClick={handleReply}>Reply</button>
+              )
+            ) : null}
+          </div>
+        );
+      }}
+    </AuthContext.Consumer>
+  );
+};
+
+export default CommentCtrls;
