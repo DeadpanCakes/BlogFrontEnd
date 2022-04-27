273e791b342ed9e5749d365b4ae0819efb28625c
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-26 17:14:40 -0700

    added DeletedComment to Comment

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import parseHtml from "html-react-parser";
 
 import CommentCtrls from "./CommentCtrls";
+import DeletedComment from "./DeletedComment";
 
 const Comment = ({
   comment,
@@ -10,14 +11,18 @@ const Comment = ({
   setEditing,
   isPreview = false,
 }) => {
+  const containerStyle = "pt-5 pl-5 border-l border-darkBG bg-[#C2CAE8]";
+
   const nestedComments = (comment.children || []).map((comment) => {
     return <Comment key={comment._id} comment={comment} setReply={setReply} />;
   });
-  return (
-    <li
-      id={comment._id}
-      className="pt-5 pl-5 border-l border-darkBG bg-[#C2CAE8]"
-    >
+  return comment.isPlaceholder ? (
+    <li className={containerStyle}>
+      <DeletedComment className={containerStyle} />
+      <ul>{nestedComments}</ul>
+    </li>
+  ) : (
+    <li id={comment._id} className={containerStyle}>
       <div>
         <h3>
           <Link to={comment.author.fetchUrl}>{comment.author.username}</Link>
