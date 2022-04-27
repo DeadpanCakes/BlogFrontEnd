180347a8a9346f572b9e06f647ae7652f9d08d04
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-04-26 17:14:26 -0700

    made component to act as placeholders for deleted comments with children

---
+++ src/components/comment/DeletedComment.js
@@ -0,0 +1,14 @@
+const DeletedComment = () => {
+  return (
+    <>
+      <div>
+        <h3>DELETED</h3>
+        <div>
+          <p> This comment has been deleted</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DeletedComment;
